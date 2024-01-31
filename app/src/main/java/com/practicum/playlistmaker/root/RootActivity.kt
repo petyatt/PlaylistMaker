68ab0ad24396cd6f76a0fb471c133b5bf02b3470
Petyat <petya.07@ayndex.ru>
2024-01-31 14:58:48 +0700

    New commit(19sprint(исправления))

--- app/src/main/java/com/practicum/playlistmaker/root/RootActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/root/RootActivity.kt
@@ -1,11 +1,8 @@
 package com.practicum.playlistmaker.root
 
-import android.graphics.Rect
 import android.os.Bundle
 import android.view.View
-import android.view.ViewTreeObserver
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.practicum.playlistmaker.R
@@ -13,13 +10,12 @@ import com.practicum.playlistmaker.databinding.ActivityRootBinding
 
 class RootActivity: AppCompatActivity() {
 
-    private var _binding: ActivityRootBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: ActivityRootBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        _binding = ActivityRootBinding.inflate(layoutInflater)
+        binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navHostFragment =
@@ -28,20 +24,18 @@ class RootActivity: AppCompatActivity() {
 
         binding.bottomNavigation.setupWithNavController(navController)
 
-        val rootView = findViewById<View>(android.R.id.content)
-        val bottomNavigationView = binding.bottomNavigation
-        val listener = ViewTreeObserver.OnGlobalLayoutListener {
-            val r = Rect()
-            rootView.getWindowVisibleDisplayFrame(r)
-            val screenHeight = rootView.rootView.height
-            val keypadHeight = screenHeight - r.bottom
-            bottomNavigationView.isVisible = keypadHeight <= screenHeight * 0.15
-        }
-        rootView.viewTreeObserver.addOnGlobalLayoutListener(listener)
-    }
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.player -> {
+                    binding.bottomNavigation.visibility = View.GONE
+                    binding.line.visibility = View.GONE
+                }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
+                else -> {
+                    binding.bottomNavigation.visibility = View.VISIBLE
+                    binding.line.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
